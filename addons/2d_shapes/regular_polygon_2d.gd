68e088c24ee9ad858a3e78772871d583202af588
9thAzure <dl6741@yahoo.ca>
2023-02-05 17:11:02 -0800

    regular polygon 2d: changed property documentation/comments, added a few new TBI properties, and changed everything to use pre_redraw

--- addons/2d_shapes/regular_polygon_2d.gd
+++ addons/2d_shapes/regular_polygon_2d.gd
@@ -10,16 +10,16 @@ extends Polygon2D
 	set(value):
 		assert(vertices_count >= 1)
 		vertices_count = value
-		queue_redraw()
+		pre_redraw()
 
-## The length of each corner to the center.
+## The length from each corner to the center.
 @export var size : float = 10:
 	set(value):
 		assert(size >= 0)
 		size = value
-		queue_redraw()
+		pre_redraw()
 
-# unfortunately, negative values are not working for export range
+# negative values are not working for export range
 ## The offset rotation of the shape, in degrees.
 @export_range(0, 360, 0.1, "or_greater", "or_less") var offset_rotation_degrees : float = 0:
 	set(value):
@@ -27,25 +27,16 @@ extends Polygon2D
 	get:
 		return rad_to_deg(offset_rotation)
 
-# ? debating whether the offset_* properties should have their own export_category.
 ## the offset rotation of the shape, in radians
 var offset_rotation : float = 0:
 	set(value):
 		offset_rotation = value
-		queue_redraw()
+		pre_redraw()
 
-# Todo: implement effects
-## The offset position of the shape.
-@export var offset_position := Vector2.ZERO:
-	set(value):
-		offset_position = value
-		queue_redraw()
-
-# ? not sure if this is a good name, may be changed.
+# ? not sure if this is a good name for it and many of the properties under it, they may need changing.
 @export_category("advanced")
 
-# ? not sure if this is the correct term for it or if it is a good name for it.
-# Todo: implement effects
+# Todo: implement effects, convert to "width"
 ## Creates a hole in the center of the shape, in the same shape. If "size" is set to 1 (circle), a 32 sized shape in its place.
 ## Produces a warning if it is set to a value smaller than 0 or greater than "size".
 @export var hole_size : float = 0:
@@ -57,43 +48,45 @@ var offset_rotation : float = 0:
 			value = size
 			push_warning("attempted to set variable \"hole_size\" to a value greater than the variable \"size\", value set to \"size\": %s." % size)
 		hole_size = value
-		queue_redraw()
+		pre_redraw()
 
-# Todo:
+# Todo: implement effects
 # this variable would cut out edges (eg: draw only have a hexagon, a trapezoid). To be implemented. Name is temporary.
 # Exact implementation/way of working TBD.
-# @export var shape_cut : int = 0
+## Cuts out vertices of the shape, as a percentage. Cuts are only made at the corners.
+@export_range(0, 1, 0.0001) var shape_cut_percentage : float = 0.0:
+	set(value):
+		shape_cut_percentage = value
+		self.queue_redraw()
+		pre_redraw()
 
-# Todo: change _draw to use Polygon2D's Polygon property
-# ? will this cause a infinite loop with Polygon being set, queue_redraw being called, _draw being called, etc?
-func _draw() -> void:
-	if (vertices_count == 1):
-		draw_circle(Vector2.ZERO, size, color)
-		return
-	
-	if (vertices_count == 2):
-		if offset_rotation == 0:
-			draw_line(Vector2.UP * size, Vector2.DOWN * size, color)
-			return
-		var point1 := Vector2(sin(offset_rotation), cos(offset_rotation)) * size
-		draw_line(point1, -point1, color)
-		return
-	
-	if (vertices_count == 4 && offset_rotation == 0):
-		const sqrt_two_over_two := 0.707106781
-		draw_rect(Rect2(-Vector2.ONE * sqrt_two_over_two * size, Vector2.ONE * sqrt_two_over_two * size * 2), color)
-		return
-	
-	var points := PackedVector2Array()
-	var rotation_spacing := TAU / vertices_count
-	# rotation is initialized pointing down and offset to the left so that the bottom is flat
-	var current_rotation := rotation_spacing / 2 - offset_rotation
-	for i in vertices_count:
-		points.append(Vector2(sin(current_rotation), cos(current_rotation)) * size) 
-		current_rotation += rotation_spacing
-	# ! draw_colored_polygon doesn't take into account holes unlike Polygon2D, so this will have to be redone.
-	if !is_zero_approx(hole_size):
-		add_hole_to_points(points, hole_size / size)
+# Todo: implement check, effects
+@export_range(0, 5, 0.01, "or_greater") var corner_size : float = 0.0:
+	set(value):
+		corner_size = value
+		pre_redraw()
+
+# Todo: implement effects
+@export_range(1, 8, 1, "or_greater") var corner_smoothness : int = 1:
+	set(value):
+		corner_smoothness = value
+		pre_redraw()
+
+func _ready():
+	pre_redraw()
+
+# Todo: _draw will have to be changed as "polygon" is drawn before _draw() is called.
+func pre_redraw() -> void:
+	pass
+	# var points = get_shape_vertices(vertices_count, size, offset_rotation)
+	# if !is_zero_approx(hole_size):
+	# 	add_hole_to_points(points, hole_size / size)
+
+	# polygon = points
+	# super.queue_redraw()
+
+func _draw():
+	var points = get_shape_vertices(vertices_count, size, offset_rotation)
 	draw_colored_polygon(points, color)
 
 # <section> helper functions for _draw()
@@ -118,4 +111,4 @@ static func add_hole_to_points(points : PackedVector2Array, hole_scaler : float)
 	points.append(points[0])
 	var original_size := points.size()
 	for i in original_size:
-		points.append(points[i] * hole_scaler)
+		points.append(points[original_size - i - 1] * hole_scaler)
