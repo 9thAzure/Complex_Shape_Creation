cd038a6d4a825c2a9a5e6b09a28fa1190b78b03a
9thAzure <dl6741@yahoo.ca>
2023-02-25 23:09:01 -0800

    implemented some functionality of 'drawn_arc' - it devides the shape. Need to add 'offset_rotation' and functionality with rounded corners

--- addons/2d_shapes/regular_polygon_2d.gd
+++ addons/2d_shapes/regular_polygon_2d.gd
@@ -67,6 +67,19 @@ var offset_rotation : float = 0:
 		self.queue_redraw()
 		_pre_redraw()
 
+## How much of the shape is drawn.
+@export_range(-360, 360) 
+var drawn_arc_degrees : float = 360:
+	set(value):
+		drawn_arc = deg_to_rad(value)
+	get:
+		return rad_to_deg(drawn_arc)
+
+var drawn_arc : float = TAU:
+	set(value):
+		drawn_arc = value
+		_pre_redraw()
+
 ## The length cut off from each end to form the corner.
 ## Values are clamped to 0 and half of the side length.
 @export_range(0, 5, 0.01, "or_greater") var corner_size : float = 0.0:
@@ -139,7 +152,7 @@ func _draw():
 		return
 		# no matches, using default drawing.
 
-	var points = get_shape_vertices(vertices_count, size, offset_rotation, offset)
+	var points = get_shape_vertices(vertices_count, size, offset_rotation, offset, drawn_arc)
 
 	if not is_zero_approx(corner_size):
 		points = get_rounded_corners(points, corner_size, corner_smoothness)
@@ -183,19 +196,74 @@ static func get_side_length(vertices_count : int):
 	if vertices_count == 2: return 1
 	return 2 * sin(TAU / vertices_count / 2)
 
+# ! Note: this func should be static, it was removed for debug purposes and should be re-added when done.
 ## Returns a [PackedVector2Array] with the points for drawing the specified shape with [method CanvasItem.draw_colored_polygon].
-static func get_shape_vertices(vertices_count : int, size : float = 1, offset_rotation : float = 0.0, offset_position : Vector2 = Vector2.ZERO) -> PackedVector2Array:
+func get_shape_vertices(vertices_count : int, size : float = 1, offset_rotation : float = 0.0, offset_position : Vector2 = Vector2.ZERO, drawn_arc : float = TAU) -> PackedVector2Array:
 	assert(vertices_count > 0)
 	assert(size > 0)
 
 	var points := PackedVector2Array()
-	var rotation_spacing := TAU / vertices_count
-	# rotation is initialized pointing down and offset to the left so that the bottom is flat
-	var current_rotation := rotation_spacing / 2 - offset_rotation
-	for i in vertices_count:
-		points.append(Vector2(sin(current_rotation), cos(current_rotation)) * size + offset_position) 
+	var sign := signf(drawn_arc)
+	var rotation_spacing := TAU / vertices_count * sign
+	
+	# If drawing a full shape
+	if drawn_arc >= TAU or drawn_arc <= -TAU:
+		# rotation is initialized pointing down and offset to the left so that the bottom is flat
+		var current_rotation := rotation_spacing / 2 - offset_rotation
+		for i in vertices_count:
+			points.append(_get_vertices(current_rotation, size, offset_position))
+			current_rotation += rotation_spacing
+		return points
+			
+	# Drawing a partial shape.
+	var current_rotation := -rotation_spacing / 2 
+	# draw_circle(_get_vertices(current_rotation, size, offset_position), 0.5, Color.GRAY)
+	var limit := sign * drawn_arc
+	var fail_safe := 0
+	while sign * current_rotation < limit:
+		fail_safe += 1
+		assert(fail_safe < 100, "fail_safe: infinite loop!")
+		var point := _get_vertices(current_rotation, size, offset_position)
+		points.append(point)
 		current_rotation += rotation_spacing
+	
+	# Setting center point.
+	var first_point := points[0]
+	var vector_to_second := (points[1] if points.size() != 1 else _get_vertices(current_rotation, size, offset_position)) - first_point
+	# draw_line(first_point, first_point + vector_to_second, Color.BLACK)
+	# draw_circle(first_point, 0.1, Color.GRAY)
+	points[0] = first_point + vector_to_second / 2
+
+	if is_equal_approx(current_rotation, drawn_arc):
+		points.append(_get_vertices(current_rotation, size, offset_position)) 
+	else:
+		# variable names in formula: P, _, Q, R
+		var last_point := points[-1]
+		var next_point := _get_vertices(current_rotation, size, offset_position)
+		var edge_slope := next_point - last_point 
+		var ending_slope := Vector2(sin(-drawn_arc), cos(-drawn_arc))
+		# formula variables: P, Q, S, R
+		var scaler := _find_intersection(last_point, edge_slope, offset_position, ending_slope)
+		var edge_point := last_point + scaler * edge_slope
+		# draw_line(last_point, last_point + edge_slope, Color.ORANGE)
+		# draw_line(offset_position, offset_position + ending_slope * size, Color.GREEN)
+		points.append(edge_point)
+	
+	if not is_equal_approx(drawn_arc, PI):
+		points.append(offset_position)
+			
 	return points
+
+static func _get_vertices(rotation : float, size : float, offset : Vector2) -> Vector2:
+	return Vector2(-sin(rotation), cos(rotation)) * size + offset
+
+# finds the intersection between 2 points and their slopes. The value returned is not the point itself, but a scaler.
+# The point would be obtained by (where a = returned value of function): point1 + a * slope1
+static func _find_intersection(point1 : Vector2, slope1 : Vector2, point2: Vector2, slope2: Vector2) -> float:
+	var numerator := slope2.y * (point2.x - point1.x) - slope2.x * (point2.y - point1.y)
+	var devisor := (slope1.x * slope2.y) - (slope1.y * slope2.x)
+	return numerator / devisor 
+	
 
 # Todo: Check what happens when corner_size is greater then halve the side lengths (corners are bigger than the shape).
 ## Returns a new [PackedVector2Array] with the points for drawing the rounded shape with [method CanvasItem.draw_colored_polygon].
@@ -225,7 +293,7 @@ static func get_rounded_corners(points : PackedVector2Array, corner_size : float
 		new_points[i * index_factor] = starting_point
 		new_points[i * index_factor + index_factor - 1] = ending_point
 
-		# Quadratic Bezier curves are use because we have all three required points already. It isn't perfect though.
+		# Quadratic Bezier curves are use because we have all three required points already. It isn't a perfect circle, but good enough.
 		# sub_i is initialized with a value of 1 as a corner_smoothness of 1 has no between points.
 		var sub_i := 1
 		while sub_i < corner_smoothness:
