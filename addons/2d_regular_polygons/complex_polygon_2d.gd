de3b3b4885e62d91de8c0596eb7614548d1a4889
9thAzure <dl6741@yahoo.ca>
2023-04-04 20:07:29 -0700

    used constants for values known ahead of time with ifs.

--- addons/2d_regular_polygons/complex_polygon_2d.gd
+++ addons/2d_regular_polygons/complex_polygon_2d.gd
@@ -235,13 +235,13 @@ func draw_using_polygon():
 	var uses_drawn_arc := _uses_drawn_arc()
 	var points = get_shape_vertices(vertices_count, size, offset_rotation, Vector2.ZERO, drawn_arc, not uses_width)
 	if uses_width and uses_drawn_arc:
-		add_hole_to_points(points, 1 - width / size, not uses_drawn_arc)
+		add_hole_to_points(points, 1 - width / size, false)
 
 	if not is_zero_approx(corner_size):
 		get_rounded_corners(points, corner_size, corner_smoothness if corner_smoothness != 0 else 32 / vertices_count)
 
 	if uses_width and not uses_drawn_arc:
-		add_hole_to_points(points, 1 - width / size, not uses_drawn_arc)
+		add_hole_to_points(points, 1 - width / size, true)
 	
 	polygon = points
 
