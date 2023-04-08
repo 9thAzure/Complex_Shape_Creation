28e45ccef6a908b07a74b04df12285a09babd5db
9thAzure <dl6741@yahoo.ca>
2023-04-07 18:31:36 -0700

    fixes to get_shape_vertices

--- addons/2d_regular_polygons/regular_polygon_2d.gd
+++ addons/2d_regular_polygons/regular_polygon_2d.gd
@@ -86,13 +86,13 @@ static func get_shape_vertices(vertices_count : int, size : float = 1, offset_ro
 	assert(vertices_count >= 1, "param 'vertices_count' must be 1 or greater.")
 	assert(size > 0, "param 'size' must be positive.")
 	
-	if vertices_count == 0:
+	if vertices_count == 1:
 		vertices_count = 32
 
 	var points := PackedVector2Array()
 	points.resize(vertices_count)
 	var rotation_spacing := TAU / vertices_count
-	var current_rotation := rotation_spacing / 2 + offset_rotation
+	var current_rotation := -rotation_spacing / 2 + offset_rotation
 	for i in vertices_count:
 		points[i] = Vector2(-sin(current_rotation), cos(current_rotation)) * size + offset_position
 		current_rotation += rotation_spacing
