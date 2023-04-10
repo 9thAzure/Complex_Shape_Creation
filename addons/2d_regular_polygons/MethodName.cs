af289fe30fbf9e00e8081e7ee75f2d84d83e4ae2
9thAzure <dl6741@yahoo.ca>
2023-04-10 11:57:25 -0700

    Added static classes to hold member names

---
+++ addons/2d_regular_polygons/MethodName.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+namespace RegularPolygons2D.MemberNames;
+
+public static class MethodNames
+{
+    public static readonly StringName GetShapeVertices = new("get_shape_vertices");
+    public static readonly StringName DrawUsingPolygon = new("draw_using_polygon");
+    public static readonly StringName UsesPolygonMember = new("uses_polygon_member");
+    public static readonly StringName GetSideLength = new("get_side_length");
+    public static readonly StringName AddRoundedCorners = new("add_rounded_corners");
+    public static readonly StringName QuadraticBezierInterpolate = new("quadratic_bezier_interpolate");
+    public static readonly StringName AddHoleToPoints = new("add_hole_to_points");
+    public static readonly StringName QueueRegenerate = new("queue_regenerate");
+    public static readonly StringName Regenerate = new("regenerate");
+    
+}
