4b6bac48d44112449e9ec8a4d0c336fbfc41c103
9thAzure <dl6741@yahoo.ca>
2023-04-15 20:50:55 -0700

    added documentation comments.

--- addons/2d_regular_polygons/SimplePolygon2D.cs
+++ addons/2d_regular_polygons/SimplePolygon2D.cs
@@ -5,64 +5,88 @@ using RegularPolygons2D.MemberNames;
 
 namespace RegularPolygons2D;
 
+/// <summary>
+/// A Wrapper for a <see cref="Node2D"/> which contains the script specified in <see cref="SimplePolygon2D.GDScriptEquivalent"/>.
+/// It also provides ways for creating such nodes.
+/// </summary>
 public partial class SimplePolygon2D
 {
+    /// <summary>The string path to the script this class wraps around.</summary>
     public const string GDScriptEquivalentPath = "res://addons/2d_regular_polygons/simple_polygon_2d.gd";
+    /// <summary>The loaded <see cref="GDScript"/> of <see cref="GDScriptEquivalentPath"/>.</summary>
     public static readonly GDScript GDScriptEquivalent = GD.Load<GDScript>(GDScriptEquivalentPath);
     private static readonly Lazy<Node2D> _shared = new(() => GDScriptEquivalent.New().As<Node2D>());
+    /// <summary>The <see cref="GDScript"/> instance this class wraps around.</summary>
     public Node2D Instance { get; }
 
+    /// <summary>
+    /// The number of vertices in the perfect shape. A value of <c>1</c> creates a circle, and a value of <c>2</c> creates a line.
+    /// Values are clamped to a value greater than or equal to <c>1</c>.
+    /// </summary>
     public long VerticesCount
     {
         get => (long)Instance.Get(PropertyName.VerticesCount);
         set => Instance.Set(PropertyName.VerticesCount, value);
     }
+    /// <summary>The length from each corner to the center. Values are clamped to a value greater than <c>0</c>.</summary>
     public double Size
     {
         get => (double)Instance.Get(PropertyName.Size);
         set => Instance.Set(PropertyName.Size, value);
     }
+    /// <summary>The offset rotation of the shape, in degrees.</summary>
     public double OffsetRotationDegrees
     {
         get => (double)Instance.Get(PropertyName.OffsetRotationDegrees);
         set => Instance.Set(PropertyName.OffsetRotationDegrees, value);
     }
+    /// <summary>The offset rotation of the shape, in radians.</summary>
     public double OffsetRotation
     {
         get => (double)Instance.Get(PropertyName.OffsetRotation);
         set => Instance.Set(PropertyName.OffsetRotation, value);
     }
+    /// <summary>The color of the shape.</summary>
     public Color Color
     {
         get => (Color)Instance.Get(PropertyName.Color);
         set => Instance.Set(PropertyName.Color, value);
     }
+    /// <summary>The offset position of the shape.</summary>
     public Vector2 OffsetPosition
     {
         get => (Vector2)Instance.Get(PropertyName.OffsetPosition);
         set => Instance.Set(PropertyName.OffsetPosition, value);
     }
+    /// <summary>Position, relative to the node's parent.</summary>
     public Vector2 Position
     {
         get => Instance.Position; 
         set => Instance.Position = value;
     }
+    /// <summary>Rotation in radians, relative to the node's parent.</summary>
     public float Rotation
     {
         get => Instance.Rotation;
         set => Instance.Rotation = value;
     }
+    /// <summary>Helper property to access <see cref="Node2D.Rotation"/> in degrees instead of radians.</summary>
     public float RotationDegrees
     {
         get => Instance.RotationDegrees;
         set => Instance.RotationDegrees = value;
     }
+    /// <summary>The node's scale. Unscaled value: (1, 1).</summary>
     public Vector2 Scale
     {
         get => Instance.Scale;
         set => Instance.Scale = value;
     }
 
+    /// <summary>Creates and wraps a <see cref="SimplePolygon2D"/> around <paramref name="instance"/>.</summary>
+    /// <param name="instance">The instance of <see cref="GDScriptEquivalent"/> to wrap.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="instance"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="instance"/> isn't a instance of <see cref="GDScriptEquivalent"/>.</exception>
     public SimplePolygon2D(Node2D instance)
     {
         if (instance is null)
@@ -72,10 +96,14 @@ public partial class SimplePolygon2D
 
         Instance = instance;
     }
+    /// <summary>Creates an instance of <see cref="GDScriptEquivalent"/> wrapped by a new <see cref="SimplePolygon2D"/>.</summary>
     public SimplePolygon2D(long verticesCount = 1, double size = 10, double offsetRotation = 0, Color? color = default, Vector2 offsetPosition = default)
     {
         Instance = SimplePolygon2D.New(verticesCount, size, offsetRotation, color, offsetPosition);
     }
+    /// <summary>
+    /// Creates an instance of <see cref="GDScriptEquivalent"/> with the specified parameters.
+    /// </summary>
     public static Node2D New(long verticesCount = 1, double size = 10, double offsetRotation = 0, Color? color = default, Vector2 offsetPosition = default)
     {
         Debug.Assert(GDScriptEquivalent is not null);
@@ -86,6 +114,8 @@ public partial class SimplePolygon2D
         return (Node2D)GDScriptEquivalent.New(verticesCount, size, offsetRotation, color.Value, offsetPosition);
     }
 
+    /// <summary>Returns an array of <see cref="Vector2"/>s with the points for the shape with the specified <paramref name="verticesCount"/>.</summary>
+    /// <param name="verticesCount">If it is <c>1</c>, a value of <c>32</c> is used.</param>
     public static Vector2[] GetShapeVertices(long verticesCount, double size = 1, double offsetRotation = 0, Vector2 offsetPosition = default)
     => _shared.Value.Call(MethodNames.GetShapeVertices, verticesCount, size, offsetRotation, offsetPosition).As<Vector2[]>();
 
