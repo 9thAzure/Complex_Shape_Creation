4d8ccf93f426641c6120a5690931a710efaf72d6
9thAzure <dl6741@yahoo.ca>
2023-04-16 21:31:59 -0700

    Property Documentation

--- addons/2d_regular_polygons/RegularCollisionPolygon2D.cs
+++ addons/2d_regular_polygons/RegularCollisionPolygon2D.cs
@@ -7,9 +7,12 @@ namespace RegularPolygons2D;
 
 public class RegularCollisionPolygon2D
 {
+    /// <inheritdoc cref="SimplePolygon2D.GDScriptEquivalentPath"/>
     public const string GDScriptEquivalentPath = "res://addons/2d_regular_polygons/regular_collision_polygon_2d.gd";
+    /// <summary>The loaded <see cref="GDScript"/> of <see cref="GDScriptEquivalentPath"/>.</summary>
     public static readonly GDScript GDScriptEquivalent = GD.Load<GDScript>(GDScriptEquivalentPath);
 
+    /// <summary>The <see cref="GDScriptEquivalent"/> instance this class wraps around.</summary>
     public CollisionShape2D Instance { get; }
 
     /// <inheritdoc cref="SimplePolygon2D.VerticesCount"/>
@@ -36,50 +39,35 @@ public class RegularCollisionPolygon2D
         get => (double)Instance.Get(PropertyName.OffsetRotation);
         set => Instance.Set(PropertyName.OffsetRotation, value);
     }
-    /// <summary>Determines the width of the shape and whether is uses Draw* methods or <see cref="Polygon2D.Polygon"/>.</summary>
+    /// <summary>Determines the width of the shape.</summary>
     /// <remarks>
-    /// A value of <c>0</c> outlines the shape with lines, and a value smaller than <c>0</c> ignores this effect.
-    /// Values greater than <c>0</c> will have <see cref="Polygon2D.Polygon"/> used,
-    /// and value greater than <see cref="size"/> also ignores this effect while still using [member Polygon2D.polygon].
-    /// A value between <c>0</c> and <c>0.01</c> is converted to <c>0</c>, to make it easier to select it in the inspector.
+    /// It only has an effect with values greater than [code]0[/code].
+    /// Values greater than or equal to [member size] force the usage of [ConvexPolygonShape2D].
     /// </remarks>
     public double Width
     {
         get => (double)Instance.Get(PropertyName.Width);
         set => Instance.Set(PropertyName.Width, value);
     }
-    /// <summary>The arc of the drawn shape, in degrees, cutting off beyond that arc.</summary>
-    /// <remarks>
-    /// Values greater than <c>360</c> or <c>-360</c> draws a full shape. It starts in the middle of the base of the shapes. 
-    /// The direction of the arc is clockwise with positive values and counterclockwise with negative values.
-    /// [br][br]A value of <c>0</c> makes the node not draw anything.
-    /// </remarks>
+    /// <inheritdoc cref="RegularPolygon2D.DrawnArcDegrees"/>
     public double DrawnArcDegrees
     {
         get => (double)Instance.Get(PropertyName.DrawnArcDegrees);
         set => Instance.Set(PropertyName.DrawnArcDegrees, value);
     }
-    /// <summary>The arc of the drawn shape, radians, cutting off beyond that arc.</summary>
-    /// <remarks>
-    /// Values greater than <see cref="Math.Tau"/> or -<see cref="Math.Tau"/> draws a full shape. It starts in the middle of the base of the shapes. 
-    /// The direction of the arc is clockwise with positive values and counterclockwise with negative values.
-    /// [br][br]A value of <c>0</c> makes the node not draw anything.
-    /// </remarks>
+    /// <inheritdoc cref="RegularPolygon2D.DrawnArc"/>
     public double DrawnArc
     {
         get => (double)Instance.Get(PropertyName.DrawnArc);
         set => Instance.Set(PropertyName.DrawnArc, value);
     }
-
-    /// <summary>The distance from each vertex along the edge to the point where the rounded corner starts.</summary>
-    /// <remarks>If this value is over half of the edge length, the mid-point of the edge is used instead.</remarks>
+    /// <inheritdoc cref="RegularPolygon2D.CornerSize"/>
     public double CornerSize
     {
         get => (double)Instance.Get(PropertyName.CornerSize);
         set => Instance.Set(PropertyName.CornerSize, value);
     }
-    /// <summary>How many lines make up each corner</summary>
-    /// <remarks>A value of <c>0</c> will use a value of <c>32</c> divided by <see cref="verticesCount"/>.</remarks>
+    /// <inheritdoc cref="RegularPolygon2D.CornerSmoothness"/>
     public long CornerSmoothness
     {
         get => (long)Instance.Get(PropertyName.CornerSmoothness);
