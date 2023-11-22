4b4ed4ac88065bac43a159d9f7abb0720b2c20a0
9thAzure <dl6741@yahoo.ca>
2023-11-21 22:01:09 -0800

    added test_init__filled_params__assigned_to_vars

--- tests/unit_tests/test_regular_collision_polygon_2d.gd
+++ tests/unit_tests/test_regular_collision_polygon_2d.gd
@@ -1,2 +1,14 @@
 extends GutTest
 
+func test_init__filled_params__assigned_to_vars():
+    var shape : RegularCollisionPolygon2D
+
+    shape = RegularCollisionPolygon2D.new(5, 5.0, 1.0, 1.0, 1.0, 1.0, 1)
+
+    assert_eq(shape.vertices_count, 5)
+    assert_eq(shape.size, 5.0)
+    assert_eq(shape.offset_rotation, 1.0)
+    assert_eq(shape.width, 1.0)
+    assert_eq(shape.drawn_arc, 1.0)
+    assert_eq(shape.corner_size, 1.0)
+    assert_eq(shape.corner_smoothness, 1)
