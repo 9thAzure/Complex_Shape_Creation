b0098b002b934762af4ec6e2540b34af46710092
9thAzure <dl6741@yahoo.ca>
2023-11-22 21:53:08 -0800

    added test_get_star_vertices__drawn_arc_PI__no_central_point

--- tests/unit_tests/test_star_polygon_2d.gd
+++ tests/unit_tests/test_star_polygon_2d.gd
@@ -53,3 +53,11 @@ func test_queue_regenerate__in_tree__delayed_shape_filled():
     assert_true(star.polygon.is_empty(), "polygon property should not be instantly filled.")
     await wait_frames(2)
     assert_false(star.polygon.is_empty(), "polygon property should be filled at this point.")
+
+func test_get_star_vertices__drawn_arc_PI__no_central_point(p = use_parameters([[PI], [-PI]])):
+    var star : PackedVector2Array
+
+    star = StarPolygon2D.get_star_vertices(5, 10, 5, p[0])
+
+    assert_almost_ne(star[-1], Vector2.ZERO, Vector2.ONE * 0.01)
+    assert_eq(star.size(), 6, "size of array should be 6.")
