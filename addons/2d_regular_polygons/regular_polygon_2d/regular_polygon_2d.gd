a6ae9238db6bf6d4ac4bf7a35e4c44cfd94cca15
9thAzure <dl6741@yahoo.ca>
2023-12-09 20:43:13 -0800

    fix using rotation instead of offset_rotation

--- addons/2d_regular_polygons/regular_polygon_2d/regular_polygon_2d.gd
+++ addons/2d_regular_polygons/regular_polygon_2d/regular_polygon_2d.gd
@@ -141,7 +141,7 @@ func _draw() -> void:
 		return
 	
 	if vertices_count == 2:
-		var point := _get_vertices(rotation, size)
+		var point := _get_vertices(offset_rotation, size)
 		if width > 0 and width < size:
 			var end_point :=  point * (1 - (width / size))
 			draw_line(point + offset, end_point + offset, color, -1.0, antialiased)
