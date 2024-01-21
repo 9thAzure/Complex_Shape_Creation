dfe98432c72362641cc9e7647954ae587badd446
9thAzure <dl6741@yahoo.ca>
2024-01-20 20:23:52 -0800

    tries to select animation that isn't RESET

--- tests/visual_tests/animation_manager.gd
+++ tests/visual_tests/animation_manager.gd
@@ -9,9 +9,16 @@ var enable_all := false:
 		enable_all = value
 		for child in get_children():
 			if child is AnimationPlayer:
+				var animations := (child as AnimationPlayer).get_animation_list()
+				var animation := ""
+				for animation_name in animations:
+					if animation_name == "" or animation_name == "RESET":
+						continue
+					animation = animation_name
+				
 				if value:
 					child.advance(-child.current_animation_position)
-					child.play()
+					child.play(animation)
 				else:
 					child.pause()
 				continue
