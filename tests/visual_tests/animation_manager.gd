98833b0d93bea92fa55bc85d09bd25d0710e8df1
9thAzure <dl6741@yahoo.ca>
2024-01-21 20:16:37 -0800

    improved reseting function and moved it to own property

--- tests/visual_tests/animation_manager.gd
+++ tests/visual_tests/animation_manager.gd
@@ -17,7 +17,6 @@ var enable_all := false:
 					animation = animation_name
 				
 				if value:
-					child.advance(-child.current_animation_position)
 					child.play(animation)
 				else:
 					child.pause()
@@ -28,3 +27,37 @@ var enable_all := false:
 				continue
 			
 			print(child, "was an unexpected node.")
+
+@export
+var reset := false:
+	get:
+		return false
+	set(_ignored):
+		for child in get_children():
+			if child is AnimationPlayer:
+				var animation_player := child as AnimationPlayer
+				animation_player.advance(-animation_player.current_animation_position)
+				continue
+			
+			if child is modulator_script:
+				var modulator := child as modulator_script
+				if modulator.node_path == null:
+					continue
+				
+				var node := modulator.get_node(modulator.node_path)
+				var script = node.get_script()
+				
+				for property in modulator.properties:
+					var property_name : String = property[0]
+
+					if script == null:
+						node.set(property_name, 0)
+					else:
+						var value = script.get_property_default_value(property_name)
+						if typeof(value) == typeof(property[1]):
+							node.set(property_name, value)
+						elif typeof(property[1]) == TYPE_COLOR:
+							node.set(property_name, Color.WHITE)
+						else:
+							node.set(property_name, property[1])
+				continue
