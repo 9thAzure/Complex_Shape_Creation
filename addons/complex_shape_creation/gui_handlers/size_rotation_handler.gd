c1f7a02439ac0a51130ab72bc86d8fde89a992a6
9thAzure <dl6741@yahoo.ca>
2024-06-13 09:58:42 -0700

    proper positioning for collision polygon with star shapes

--- addons/complex_shape_creation/gui_handlers/size_rotation_handler.gd
+++ addons/complex_shape_creation/gui_handlers/size_rotation_handler.gd
@@ -1,11 +1,31 @@
 @tool
 extends "res://addons/complex_shape_creation/gui_handlers/base_handler.gd"
 
-var is_star_shape := false
+var shape_type : int = 0
+
+const _SHAPE_SIMPLE := 1
+const _SHAPE_REGULAR := 2
+const _SHAPE_STAR := 3
+const _SHAPE_COLLISION := 4
+
 
 func _ready() -> void:
 	super._ready()
-	is_star_shape = _parent is StarPolygon2D
+	shape_type = _get_shape_type(_parent)
+	
+func _get_shape_type(shape : Node2D) -> int:
+	if shape is SimplePolygon2D:
+		return _SHAPE_SIMPLE
+	if shape is RegularPolygon2D:
+		return _SHAPE_REGULAR
+	if shape is StarPolygon2D:
+		return _SHAPE_STAR
+	if shape is RegularCollisionPolygon2D:
+		return _SHAPE_COLLISION
+			
+	printerr("unrecognized shape given: %s" % shape)
+	return 0
+
 
 func _from_parent_properties() -> void:
 	var offset_rotation : float = _parent.offset_rotation + get_rotation_offset()
@@ -19,8 +39,11 @@ func _update_properties() -> void:
 
 func get_rotation_offset() -> float:
 	var offset := 0.0
-	if not is_star_shape:
+	if not is_parent_star_shape():
 		offset += PI
 		if _parent.vertices_count != 2:
 			offset += TAU / _parent.vertices_count / 2
 	return offset
+
+func is_parent_star_shape() -> bool:
+	return shape_type == _SHAPE_STAR or shape_type == _SHAPE_COLLISION and _parent.inner_size > 0
