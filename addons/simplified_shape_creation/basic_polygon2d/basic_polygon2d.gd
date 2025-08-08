6f5149125d148b192a696d314cecaec66f6b0d93
9thAzure <dl6741@yahoo.ca>
2025-08-07 18:10:59 -0700

    removed unused code

--- addons/simplified_shape_creation/basic_polygon2d/basic_polygon2d.gd
+++ addons/simplified_shape_creation/basic_polygon2d/basic_polygon2d.gd
@@ -18,15 +18,6 @@ var vertices_count : int = 1:
 		update_configuration_warnings()
 		queue_regenerate()
 
-## The length from each corner to the center of the shape.
-#@export_range(0.000001, 10, 0.001, "or_greater", "hide_slider")
-#var size : float = 10:
-#	get:
-#		return sizes[0]
-#	set(value):
-#		sizes[0] = value
-#		queue_regenerate()
-
 @export
 var sizes : PackedFloat64Array = PackedFloat64Array([10.0]):
 	set(value):
@@ -147,13 +138,6 @@ var offset_rotation : float = 0:
 	set(value):
 		offset_rotation = value
 		queue_regenerate()
-
-## @deprecated
-## Transforms [member CollisionShape2D.shape], rotating it by [param rotation] radians and scaling it by a factor of [param scaler].
-#func apply_transformation(rotation : float, scale : float) -> void:
-	#assert(scale > 0, "param 'scale' should be positive.")
-	#offset_rotation += rotation
-	#size *= scale
 
 @export
 var offset_scale := Vector2.ONE:
@@ -559,27 +543,3 @@ func _init(vertices_count : int = 1, size := 10.0, offset_rotation := 0.0, color
 		self.color = color
 	if offset_position != Vector2.ZERO:
 		self.offset = offset_position
-
-#static var _circle := get_shape_vertices(32)
-
-### Returns a [PackedVector2Array] with the points for the shape with the specified [param vertices_count].
-### [br][br]If [param vertices_count] is [code]1[/code], a value of [code]32[/code] is used instead.
-#static func get_shape_vertices(vertices_count : int, size : float = 1, offset_rotation : float = 0.0, offset_position : Vector2 = Vector2.ZERO) -> PackedVector2Array:
-#	assert(vertices_count >= 1, "param 'vertices_count' must be 1 or greater.")
-#	assert(size > 0, "param 'size' must be positive.")
-#
-#	if vertices_count == 1:
-#		return _circle * Transform2D(-offset_rotation, Vector2.ONE * size, 0, offset_position)
-#
-#	var points := PackedVector2Array()
-#	points.resize(vertices_count)
-#	var rotation_spacing := TAU / vertices_count
-#	var current_rotation := -rotation_spacing / 2 + offset_rotation
-#	for i in vertices_count:
-#		points[i] = Vector2(-sin(current_rotation), cos(current_rotation)) * size + offset_position
-#		current_rotation += rotation_spacing
-#
-#	return points
-#
-#static func _get_vertices(rotation : float, size : float = 1, offset : Vector2 = Vector2.ZERO) -> Vector2:
-#	return Vector2(-sin(rotation), cos(rotation)) * size + offset
