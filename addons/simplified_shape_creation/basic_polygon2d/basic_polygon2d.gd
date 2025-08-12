11011cb11b90ec0b9eff27c5f54618431fac8933
9thAzure <dl6741@yahoo.ca>
2025-08-11 17:07:25 -0700

    arranged export properties, and renamed line_width -> border_width and updated documentation.

--- addons/simplified_shape_creation/basic_polygon2d/basic_polygon2d.gd
+++ addons/simplified_shape_creation/basic_polygon2d/basic_polygon2d.gd
@@ -194,11 +194,20 @@ var draw_shape := true:
 		update_configuration_warnings()
 		queue_redraw()
 
-## The width of the drawn shape, if the created shape is a line. If set to a value of [code]0[/code], two-point thin lines are drawn.
-@export_range(0, 10, 0.001, "or_greater", "hide_slider")
-var line_width : float = 0.0:
+## Toggles drawing a border around a [enum ShapeType].POLYGON.
+## [br][br]If the shape is a line, this property changes which color property is used; [member color] if [code]false[/code], [member border_color] if [code]true[/code].
+@export
+var draw_borders := false:
 	set(value):
-		line_width = value
+		draw_borders = value
+		queue_redraw()
+
+## The width of the drawn border, if the shape is a [enum ShapeType].POLYGON and [member draw_borders] is [code]true[/code].
+## The width of the drawn shape, if the shape is a line. If set to a value of [code]0[/code], two-point thin lines are drawn.
+@export_range(0, 10, 0.001, "or_greater", "hide_slider")
+var border_width : float = 0.0:
+	set(value):
+		border_width = value
 		queue_redraw()
 
 ## The color of the drawn shape.
@@ -206,14 +215,6 @@ var line_width : float = 0.0:
 var color : Color = Color.WHITE:
 	set(value):
 		color = value
-		queue_redraw()
-
-## Toggles drawing a border around a [enum ShapeType].POLYGON.
-## [br][br]If the shape is a line, this property changes which color property is used; [member color] if [code]false[/code], [member border_color] if [code]true[/code].
-@export
-var draw_borders := false:
-	set(value):
-		draw_borders = value
 		queue_redraw()
 
 ## The color of the border of the shape, and for a line shape if [member draw_borders] is [code]true[/code].
@@ -607,22 +608,22 @@ func _draw() -> void:
 				if ring_ratio < 1 and (is_equal_approx(arc_angle, TAU) or closing_method == ClosingMethod.CHORD):
 					assert(_created_shape.size() % 2 == 0)
 					var border_line := _created_shape.slice(0, _created_shape.size() / 2)
-					draw_polyline(border_line, border_color, line_width)
+					draw_polyline(border_line, border_color, border_width)
 
 					for i in border_line.size():
 						border_line[i] = _created_shape[-i - 1]
-					draw_polyline(border_line, border_color, line_width)
-				elif is_zero_approx(line_width):
+					draw_polyline(border_line, border_color, border_width)
+				elif is_zero_approx(border_width):
 					draw_polyline(_created_shape, border_color)
 					draw_line(_created_shape[-1], _created_shape[0], border_color)
 				else:
 					var border_line := _created_shape.duplicate()
 					border_line.push_back(_created_shape[0])
-					draw_polyline(border_line, border_color, line_width)
+					draw_polyline(border_line, border_color, border_width)
 		ShapeType.POLYLINE:
-			draw_polyline(_created_shape, border_color if draw_borders else color, line_width if line_width > 0 else -1)
+			draw_polyline(_created_shape, border_color if draw_borders else color, border_width if border_width > 0 else -1)
 		ShapeType.MULTILINE:
-			draw_multiline(_created_shape, border_color if draw_borders else color, line_width if line_width > 0 else -1)
+			draw_multiline(_created_shape, border_color if draw_borders else color, border_width if border_width > 0 else -1)
 		_:
 			assert(false, "unexpected match case: %s" % get_created_shape_type())
 
