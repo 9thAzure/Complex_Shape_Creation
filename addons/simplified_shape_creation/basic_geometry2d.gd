1b07a78a4f867383e059c0862959bc9e2e6aa86e
9thAzure <dl6741@yahoo.ca>
2025-08-19 18:23:25 -0700

    updated warning messages of singleton

--- addons/simplified_shape_creation/basic_geometry2d.gd
+++ addons/simplified_shape_creation/basic_geometry2d.gd
@@ -3,9 +3,9 @@ class_name BasicGeometry2D
 
 ## Holds methods for creating and modifying shapes.
 
-func _init(instantiated_from_cs_singleton : bool):
+func _init(instantiated_from_cs_singleton : bool = false):
 	if !instantiated_from_cs_singleton:
-		printerr("This class is meant to be a singleton, and cannot be instantiated")
+		push_error("This class is meant to be a singleton, and cannot be instantiated. Freeing self.")
 		self.free()
 
 # gets the point on a unit circle for the specified rotation.
@@ -199,7 +199,7 @@ class SizeIncrease:
 	extends Object
 
 	func _init():
-		printerr("This class is meant to be a singleton, and cannot be instantiated")
+		push_error("This class is meant to be a singleton, and cannot be instantiated. Freeing self")
 		self.free()
 
 	## Designates how much [method RegularGeometry2D.add_rounded_corners] expands the array.
